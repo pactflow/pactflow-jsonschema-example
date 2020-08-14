5f0b24cfdfa469f2bd6457f922b476340f225b2e
Matt Fellows <matt.fellows@onegeek.com.au>
2020-08-14 17:27:40 +1000

    fix: create pacts directory if non-existent

--- scripts/generate_contract.sh
+++ scripts/generate_contract.sh
@@ -2,5 +2,6 @@
 PACT_FILE="${PWD}/consumer/pacts/schema-consumer_schema-provider.json"
 TEMPLATE_SCHEMA="${PWD}/pact-template.json"
 
+mkdir -p consumer/pacts
 BODY=$(./node_modules/.bin/typescript-json-schema "consumer/*.ts" "*")
 cat ${TEMPLATE_SCHEMA} | jq ".schema += ${BODY}" > ${PACT_FILE}
