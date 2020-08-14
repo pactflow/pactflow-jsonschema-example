1190bd718370bc2699027e1fa475d035d66ba8d5
Matt Fellows <matt.fellows@onegeek.com.au>
2020-08-14 18:16:27 +1000

    feat: update contract

--- scripts/generate_contract.sh
+++ scripts/generate_contract.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 PACT_FILE="${PWD}/consumer/pacts/schema-consumer_schema-provider.json"
 TEMPLATE_SCHEMA="${PWD}/pact-template.json"
-BODY=$(./node_modules/.bin/typescript-json-schema "consumer/*.ts" "*")
+BODY=$(./node_modules/.bin/typescript-json-schema --required "consumer/product.ts" "Product")
 cat ${TEMPLATE_SCHEMA} | jq ".schema += ${BODY}" > ${PACT_FILE}
